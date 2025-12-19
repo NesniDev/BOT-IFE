55af126fd5e2fe3154c015d3eab01aeafbc178d2
Neider Nieto <nestnidev17@gmail.com>
2025-12-19 15:11:49 -0500

    add images to send

--- src/actions/courses/veterinaria/veterinaria.js
+++ src/actions/courses/veterinaria/veterinaria.js
@@ -3,12 +3,32 @@ import whatsappServices from '#services/whatsappServices.js'
 class CourseVeterinaria {
   async sendCourseVeterinaria(to) {
     await this.sendImageDocInscripcionVeterinaria(to)
+    await this.sendImageCostsVeterinaria(to)
+    await this.sendImageSchedulesVeterinaria(to)
   }
 
   async sendImageDocInscripcionVeterinaria(to) {
     const imageUrl =
-      'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2 '
+      'https://drive.google.com/uc?export=download&id=1FSyI_bTwd1xElCFuI0bqdjGNW1EuTdGt'
+
     const caption = 'Documento de Inscripción Veterinaria'
+    const type = 'image'
+    await whatsappServices.sendMediaMessage(to, type, imageUrl, caption)
+  }
+
+  async sendImageCostsVeterinaria(to) {
+    const imageUrl =
+      'https://drive.google.com/uc?export=download&id=10f6hU_iRP4r3FANEFX8xagcLID1_-t5w'
+
+    const caption = 'Costos del tecnico de veterinaria'
+    const type = 'image'
+    await whatsappServices.sendMediaMessage(to, type, imageUrl, caption)
+  }
+
+  async sendImageSchedulesVeterinaria(to) {
+    const imageUrl =
+      'https://drive.google.com/uc?export=download&id=14PPE7DxxsSTTJOch1XvOX4lLr-Aaj8F4'
+    const caption = 'Horarios del tecnico de veterinaria'
     const type = 'image'
     await whatsappServices.sendMediaMessage(to, type, imageUrl, caption)
   }
