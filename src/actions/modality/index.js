915541c2512ac2a23c8743b5c124e770e1350c9f
Neider Nieto <nestnidev17@gmail.com>
2025-12-18 19:11:24 -0500

    add menu modality

--- src/actions/modality/index.js
+++ src/actions/modality/index.js
@@ -1,7 +1,26 @@
+import whatsappService from '#services/whatsappServices.js'
+import CoursesListInOffice from './in-office/index.js'
+import CoursesListRemote from './remote/index.js'
 class OptionSelectedModality {
   async optionSelected(to, option) {
-    const response = `${option} ha sido seleccionado`
+    let response
+
+    switch (option) {
+      case '1':
+        await CoursesListInOffice.sendCoursesListInOffice(to)
+        break
+      case '2':
+        await CoursesListRemote.sendCoursesListRemote(to)
+        break
+      default:
+        response = await whatsappService.sendMessage(
+          to,
+          'Saluda a nuestro bot de whatsapp, no has elegido ninguna opción'
+        )
+        break
+    }
     await whatsappService.sendMessage(to, response)
+    await whatsappService.markAsRead(to)
   }
 }
 
