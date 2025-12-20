922783e78833aa2d4849ad66b65b1276de567189
Neider Nieto <nestnidev17@gmail.com>
2025-12-19 23:30:52 -0500

    add course list

---
+++ src/utils/inOffice.js
@@ -0,0 +1,50 @@
+const coursesList = {
+  1: {
+    title: 'Técnico en Asistente de Veterinaria',
+    images: {
+      image_docs:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+      image_costs:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+      image_schedules:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+    },
+    voucher: {
+      image_voucher:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+      caption_voucher: 'Voucher de inscripción'
+    },
+    captions: {
+      image_docs: 'Documento de Inscripción Veterinaria',
+      image_costs: 'Costos del técnico de veterinaria',
+      image_schedules: 'Horarios del técnico de veterinaria'
+    },
+    audio:
+      'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+  },
+  2: {
+    title: 'Técnico en Asistente de Administrativo',
+    images: {
+      image_docs:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+      image_costs:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+      image_schedules:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+    },
+    voucher: {
+      image_voucher:
+        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+      caption_voucher: 'Voucher de inscripción'
+    },
+    captions: {
+      image_docs: 'Documento de Inscripción Administrativo',
+      image_costs: 'Costos del técnico de Administrativo',
+      image_schedules: 'Horarios del técnico de Administrativo'
+    },
+    audio:
+      'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+  }
+}
+
+export default coursesList
