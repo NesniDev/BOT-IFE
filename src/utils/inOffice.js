ddc6d44502f83b7a4aef3de9bf56781955e852a0
Neider Nieto <nestnidev17@gmail.com>
2025-12-20 13:24:50 -0500

    add curse asistente administrativo

--- src/utils/inOffice.js
+++ src/utils/inOffice.js
@@ -2,12 +2,10 @@ const coursesList = {
   1: {
     title: 'Técnico en Asistente de Veterinaria',
     images: {
-      image_docs:
-        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
       image_costs:
-        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+        'https://drive.google.com/uc?export=download&id=10f6hU_iRP4r3FANEFX8xagcLID1_-t5w',
       image_schedules:
-        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+        'https://drive.google.com/uc?export=download&id=14PPE7DxxsSTTJOch1XvOX4lLr-Aaj8F4'
     },
     voucher: {
       image_voucher:
@@ -15,22 +13,19 @@ const coursesList = {
       caption_voucher: 'Voucher de inscripción'
     },
     captions: {
-      image_docs: 'Documento de Inscripción Veterinaria',
       image_costs: 'Costos del técnico de veterinaria',
-      image_schedules: 'Horarios del técnico de veterinaria'
+      image_schedules: 'Horarios del técnico de Veterinaria'
     },
     audio:
-      'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+      'https://drive.google.com/uc?export=download&id=1_E6U_kTp_OqJrBZKBniS4P1Gx2kKGJH7'
   },
   2: {
-    title: 'Técnico en Asistente de Administrativo',
+    title: 'Técnico en Asistente Administrativo',
     images: {
-      image_docs:
-        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
       image_costs:
-        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2',
+        'https://drive.google.com/uc?export=download&id=10Sn9J_gSMUMomBpy9noX5DYtmrOaQOZ9',
       image_schedules:
-        'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+        'https://drive.google.com/uc?export=download&id=1ATyj4LeXNSN0IP5fsnhhrkanV-3pARdA'
     },
     voucher: {
       image_voucher:
@@ -38,12 +33,11 @@ const coursesList = {
       caption_voucher: 'Voucher de inscripción'
     },
     captions: {
-      image_docs: 'Documento de Inscripción Administrativo',
-      image_costs: 'Costos del técnico de Administrativo',
-      image_schedules: 'Horarios del técnico de Administrativo'
+      image_costs: 'Costos del técnico dministrativo',
+      image_schedules: 'Horarios del técnico administrativo'
     },
     audio:
-      'https://drive.google.com/uc?export=download&id=1hn_QDGI44C3SakNvMhY6iMLAiXqIi9z2'
+      'https://drive.google.com/uc?export=download&id=1_E6U_kTp_OqJrBZKBniS4P1Gx2kKGJH7'
   }
 }
 
