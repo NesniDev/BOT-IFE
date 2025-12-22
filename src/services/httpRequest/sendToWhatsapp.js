7d360241a7a8a86ab05c172af874817cd6b1e9db
Neider Nieto <nestnidev17@gmail.com>
2025-12-22 18:52:55 -0500

    add structure for scalable flows

---
+++ src/services/httpRequest/sendToWhatsapp.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import config from '#config/env.js'
+
+const sendToWhatsapp = async (data) => {
+  const baseUrl = `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`
+  const headers = {
+    Authorization: `Bearer ${config.API_TOKEN}`
+  }
+
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: baseUrl,
+      headers,
+      data
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error sending message to WhatsApp:', error)
+  }
+}
+
+export default sendToWhatsapp
--- src/services/whatsappServices.js
+++ src/services/whatsappServices.js
@@ -1,154 +1,91 @@
-import axios from 'axios'
-import config from '#config/env.js'
+import sendToWhatsapp from './httpRequest/sendToWhatsapp.js'
 
 class WhatsAppService {
   async sendMessage(to, body) {
-    try {
-      if (!body || typeof body !== 'string' || !body.trim()) {
-        console.warn('sendMessage aborted: invalid text', body)
-        return
-      }
-      await axios({
-        method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
-        headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`
-        },
-        data: {
-          messaging_product: 'whatsapp',
-          to,
-          text: { body }
-          // context: {
-          //   message_id: messageId
-          // }
-        }
-      })
-    } catch (error) {
-      console.error(
-        'WhatsApp Message error:',
-        error.response?.data || error.message
-      )
+    if (!body || typeof body !== 'string' || !body.trim()) {
+      console.warn('sendMessage aborted: invalid text', body)
+      return
     }
+    const data = {
+      messaging_product: 'whatsapp',
+      to,
+      type: 'text',
+      text: {
+        body
+      }
+    }
+    await sendToWhatsapp(data)
   }
 
   async sendMediaMessage(to, type, mediaUrl, caption) {
-    try {
-      const mediaObject = {}
+    const mediaObject = {}
 
-      switch (type) {
-        case 'audio':
-          mediaObject.audio = {
-            link: mediaUrl
-          }
-          break
-        case 'image':
-          mediaObject.image = {
-            link: mediaUrl,
-            caption: caption
-          }
-          break
-        case 'video':
-          mediaObject.video = {
-            link: mediaUrl,
-            caption: caption
-          }
-          break
-        default:
-          throw new Error('Invalid media type')
-      }
-      await axios({
-        method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
-        headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`
-        },
-        data: {
-          messaging_product: 'whatsapp',
-          to,
-          type: type,
-          ...mediaObject
+    switch (type) {
+      case 'audio':
+        mediaObject.audio = {
+          link: mediaUrl
         }
-      })
-    } catch (error) {
-      console.error(
-        'WhatsApp media error:',
-        error.response?.data || error.message
-      )
+        break
+      case 'image':
+        mediaObject.image = {
+          link: mediaUrl,
+          caption: caption
+        }
+        break
+      case 'video':
+        mediaObject.video = {
+          link: mediaUrl,
+          caption: caption
+        }
+        break
+      default:
+        throw new Error('Invalid media type')
     }
+
+    const data = {
+      messaging_product: 'whatsapp',
+      to,
+      type: type,
+      ...mediaObject
+    }
+
+    await sendToWhatsapp(data)
   }
 
   async sendButtonMessage(to, buttons, bodyText) {
-    try {
-      await axios({
-        method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
-        headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`
-        },
-        data: {
-          messaging_product: 'whatsapp',
-          to,
-          type: 'interactive',
-          interactive: {
-            type: 'button',
-            body: { text: bodyText },
-            action: {
-              buttons: buttons
-            }
-          }
+    const data = {
+      messaging_product: 'whatsapp',
+      to,
+      type: 'interactive',
+      interactive: {
+        type: 'button',
+        body: { text: bodyText },
+        action: {
+          buttons: buttons
         }
-      })
-    } catch (error) {
-      console.error(
-        'WhatsApp SendButton error:',
-        error.response?.data || error.message
-      )
+      }
     }
+    await sendToWhatsapp(data)
   }
 
   async sendContacts(to, contact) {
-    try {
-      await axios({
-        method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
-        headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`
-        },
-        data: {
-          messaging_product: 'whatsapp',
-          to,
-          type: 'contacts',
-          contacts: [contact]
-        }
-      })
-    } catch (error) {
-      console.error(
-        'WhatsApp SendButton error:',
-        error.response?.data || error.message
-      )
+    const data = {
+      messaging_product: 'whatsapp',
+      to,
+      type: 'contacts',
+      contacts: [contact]
     }
+
+    await sendToWhatsapp(data)
   }
 
   async markAsRead(messageId) {
-    try {
-      await axios({
-        method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
-        headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`
-        },
-        data: {
-          messaging_product: 'whatsapp',
-          status: 'read',
-          message_id: messageId
-        }
-      })
-    } catch (error) {
-      console.error(
-        'WhatsApp markAsRead error:',
-        error.response?.data || error.message
-      )
+    const data = {
+      messaging_product: 'whatsapp',
+      status: 'read',
+      message_id: messageId
     }
+    await sendToWhatsapp(data)
   }
 }
 
